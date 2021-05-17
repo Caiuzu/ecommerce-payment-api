702075dd7f55424139bfd3f6d023a071d74d9e37
Caio Souza <caio.7s@outlook.com>
2021-05-16 21:00:46 -0300

    feat: Criando PaymentRepository

---
+++ src/main/java/br/com/ecommerce/payment/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package br.com.ecommerce.payment.repository;
+
+import br.com.ecommerce.payment.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+}
